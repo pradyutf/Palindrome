3f5ea1add932ecc231c1ac81d71ea8710271ddf6
pradyutf <pradyutfogla@gmail.com>
2024-12-10 19:29:11 +0530

    prisma init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,42 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+// routes needed
+
+app.post('/signup',(c)=>{
+  return c.text("signup page")
+})
+
+
+app.post('/signin',(c)=>{
+  return c.text("signin page")
+})
+
+
+app.post('/blog',(c)=>{
+  return c.text("create blog")
+})
+
+
+app.put('/blog',(c)=>{
+  return c.text("edit blogs")
+})
+
+
+app.get('/blog/:id',(c)=>{
+  return c.text("get a particular blog")
+})
+
+
+app.get('/blogs',(c)=>{
+ return c.text("all blogs")
+})
+ 
+
+app.get('/my-blogs',(c)=>{
+ return c.text("all blogs of a user")
+})
+
 
 
 export default app
